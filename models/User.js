df5e982d27206bd4e66ac248eaefee4df9e1b3ca
Behzad Koulivand <behzadk.1245@gmail.com>
2023-08-01 23:26:25 +0330

    User model is created

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: [true, "نام و نام خانوادگی الزامی می باشد"],
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 255,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+module.exports = mongoose.model("User", userSchema);
