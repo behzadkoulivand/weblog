6d5d222b69740ed500d7d18b8a2c8e46ddf0c1f3
Behzad Koulivand <behzadk.1245@gmail.com>
2023-08-03 00:07:08 +0330

    Errors in createUser controlled

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,25 @@
 const User = require('../models/User');
 
-exports.createUser = async (req, res)=> {
-    
-    await User.userValidation(req.body);
+exports.createUser = async (req, res, next) => {
+    try {
+        await User.userValidation(req.body);
 
-    const {fullname, email, password} = req.body
+        const { fullname, email, password } = req.body
 
-    const user = await User.findOne({email});
+        const user = await User.findOne({ email });
 
-    if(user)
-        return res.status(422).json("کاربری با این ایمیل موجود است");
+        if (user){
+            // return res.status(422).json("کاربری با این ایمیل موجود است");
+            const error = new Error("کاربری با این ایمیل موجود است");
+            error.statusCode = 422;
+            throw error;
+        }
+            
+        await User.create({ fullname, email, password });
+        res.status(200).json("عضویت با موفقیت انجام شد");
+    } catch (err) {
+        next(err);
+    }
 
-    await User.create({fullname, email, password});
-    res.status(200).json("عضویت با موفقیت انجام شد");
+
 }
